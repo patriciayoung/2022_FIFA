234df93a5e9dc3d1bddf2f1cf5888160f68c2eb0
Patricia Young <patriciayoung@Patricias-MacBook-Air.local>
2024-01-16 21:10:39 -0500

    sql tables updated

--- player stats and position stats.sql
+++ player stats and position stats.sql
@@ -4,20 +4,23 @@ drop table PLAYER_STATS;
 
 CREATE TABLE Player_Info (
     Player_id INT PRIMARY KEY,
-    PLAYER VARCHAR(50),
+    Position_id INT,
+    Player VARCHAR(50),  -- Changed the column name to PlayerName
     TEAM VARCHAR(25),
-    AGE INT
+    AGE INT,
+    FOREIGN KEY (Position_id) REFERENCES POSITION(Position_id)
 );
 
 
 CREATE TABLE POSITION (
+	Position_id INT PRIMARY KEY,
 	POSITION VARCHAR(5),
 	TEAM VARCHAR(25)
 );
 
 
 CREATE TABLE Player_Stats (
-	PLAYER_id INT PRIMARY KEY,
+	Player_id INT PRIMARY KEY,
 	TOUCHES INT, 
 	DRIBBLES_COMPLETED INT, 
 	GOALS INT, 
